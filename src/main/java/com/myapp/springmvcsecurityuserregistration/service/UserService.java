c9fbfd24ebbb261b06f72e3fe070a97b6173a64b
TrevisGreen <Trevis.Green@gmail.com>
2023-05-08 16:28:01 -0500

    Basic setup

--- src/main/java/com/myapp/springmvcsecurityuserregistration/service/UserService.java
+++ src/main/java/com/myapp/springmvcsecurityuserregistration/service/UserService.java
@@ -1,4 +1,12 @@
 package com.myapp.springmvcsecurityuserregistration.service;
 
-public interface UserService {
+import com.myapp.springmvcsecurityuserregistration.entity.User;
+import com.myapp.springmvcsecurityuserregistration.user.WebUser;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    public User findByUserName(String username);
+
+    void save(WebUser webUser);
 }
