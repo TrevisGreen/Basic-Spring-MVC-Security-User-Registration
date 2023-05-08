275f079572d02cf76de4ce16783c71bafdcd74e3
TrevisGreen <Trevis.Green@gmail.com>
2023-05-08 16:40:53 -0500

    Basic setup

--- src/main/java/com/myapp/springmvcsecurityuserregistration/dao/UserDaoImpl.java
+++ src/main/java/com/myapp/springmvcsecurityuserregistration/dao/UserDaoImpl.java
@@ -1,4 +1,45 @@
 package com.myapp.springmvcsecurityuserregistration.dao;
 
-public class UserDaoImpl {
+
+import com.myapp.springmvcsecurityuserregistration.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    UserDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public User findByUserName(String theUsername) {
+
+        // retrieve/read from database using username
+        TypedQuery<User> theQuery = entityManager.createQuery("from User where userName=:uName", User.class);
+        theQuery.setParameter("uName", theUsername);
+
+        User theUser = null;
+        try {
+            theUser = theQuery.getSingleResult();
+        } catch (Exception e) {
+            theUser = null;
+        }
+
+        return theUser;
+    }
+
+    @Override
+    @Transactional
+    public void save(User theUser) {
+
+        // create the user
+        entityManager.merge(theUser);
+    }
 }
