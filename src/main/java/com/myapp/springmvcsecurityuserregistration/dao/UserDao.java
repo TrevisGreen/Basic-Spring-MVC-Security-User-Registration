26ed89508422e2caffe9e5ce964bba38010b7ee7
TrevisGreen <Trevis.Green@gmail.com>
2023-05-08 16:33:28 -0500

    Basic setup

--- src/main/java/com/myapp/springmvcsecurityuserregistration/dao/UserDao.java
+++ src/main/java/com/myapp/springmvcsecurityuserregistration/dao/UserDao.java
@@ -1,4 +1,10 @@
 package com.myapp.springmvcsecurityuserregistration.dao;
 
+import com.myapp.springmvcsecurityuserregistration.entity.User;
+
 public interface UserDao {
+
+    User findByUserName(String username);
+
+    void save(User theUser);
 }
