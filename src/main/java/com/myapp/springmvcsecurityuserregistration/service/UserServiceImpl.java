69480ad13298759b0ba53b97eadd3ccf012a4f8c
TrevisGreen <Trevis.Green@gmail.com>
2023-05-08 16:31:30 -0500

    Basic setup

--- src/main/java/com/myapp/springmvcsecurityuserregistration/service/UserServiceImpl.java
+++ src/main/java/com/myapp/springmvcsecurityuserregistration/service/UserServiceImpl.java
@@ -1,4 +1,31 @@
 package com.myapp.springmvcsecurityuserregistration.service;
 
-public class UserServiceImpl {
+import com.myapp.springmvcsecurityuserregistration.entity.User;
+import com.myapp.springmvcsecurityuserregistration.user.WebUser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserServiceImpl implements UserService {
+
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User findByUserName(String username) {
+        return null;
+    }
+
+    @Override
+    public void save(WebUser webUser) {
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
