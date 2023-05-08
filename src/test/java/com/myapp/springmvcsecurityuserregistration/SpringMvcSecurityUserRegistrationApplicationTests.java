05d7475e7bc58b56f56fbc7443671568f175116f
TrevisGreen <Trevis.Green@gmail.com>
2023-05-08 15:59:22 -0500

    Initial commit

---
+++ src/test/java/com/myapp/springmvcsecurityuserregistration/SpringMvcSecurityUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springmvcsecurityuserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
