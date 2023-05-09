38665494611b6058500ea09bca81fec92c6fe350
TrevisGreen <Trevis.Green@gmail.com>
2023-05-08 23:56:16 -0500

    Basic setup

--- src/main/java/com/myapp/springmvcsecurityuserregistration/dao/RoleDao.java
+++ src/main/java/com/myapp/springmvcsecurityuserregistration/dao/RoleDao.java
@@ -1,4 +1,8 @@
 package com.myapp.springmvcsecurityuserregistration.dao;
 
+import com.myapp.springmvcsecurityuserregistration.entity.Role;
+
 public interface RoleDao {
+
+    public Role findRoleByName(String theRoleName);
 }
